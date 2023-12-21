4b64c1900a253b5c1095e8614a6e7a7728f88d68
RyperoneR <145667779+RyperoneR@users.noreply.github.com>
2023-12-21 13:18:25 +0100

    Create poker.py

---
+++ poker.py
@@ -0,0 +1,11 @@
+import random as rand
+import PySimpleGUI as sg 
+
+layout = [
+    [sg.Button("Abandonar")]
+    [sg.Button("Pasar")],
+    [sg.Button("Subir Apuesta")],
+    [sg.Button("All-in")]
+]
+
+ventana = sg.Window("POKER")
